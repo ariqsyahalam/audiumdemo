74ec8599aa66d27a1179fe0ac9019baf543da24d
Ariq Syahalam <70618885+ariqsyahalam@users.noreply.github.com>
2024-11-23 05:11:44 +0700

    Initial Commit

---
+++ audiumdemo/audiumdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  audiumdemoApp.swift
+//  audiumdemo
+//
+//  Created by Reyhan Ariq Syahalam on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct audiumdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
